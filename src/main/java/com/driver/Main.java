dd0b66e32bd6092a94ec0fbc90d75b2f7e2fcbbb
isaacMjr <magamboisaac9@gmail.com>
2023-06-05 15:02:04 +0300

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,20 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+   public static void main(String[] args){
+        Product p = new Product();
+       System.out.println(p.product(1.2,2.4));
+
+   }
+
 }
