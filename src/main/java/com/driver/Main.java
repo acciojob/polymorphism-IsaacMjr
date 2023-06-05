05afc23b1667182912b3a53436d58457a36ee547
isaacMjr <magamboisaac9@gmail.com>
2023-06-05 15:14:08 +0300

    second trial

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,11 +10,18 @@ public class Main {
         public double product(double x, double y){
             return x*y;
         }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
     }
 
    public static void main(String[] args){
         Product p = new Product();
-       System.out.println(p.product(1.2,2.4));
+        p.product(1,2);
+        System.out.println(p.product(1,2));
+        p.product(1,2,3);
+        System.out.println(p.product(1,2,3));
 
    }
 
